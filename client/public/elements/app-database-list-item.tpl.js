474d31e62102ebbf5f261d2bd5eed9827c0680bc
Leigh <ladragoon@ucdavis.edu>
2020-06-08 15:45:23 -0700

    Nightly saver

--- client/public/elements/app-database-list-item.tpl.js
+++ client/public/elements/app-database-list-item.tpl.js
@@ -6,8 +6,19 @@ return html`
 <style>
   :host {
     display: block;
-    padding: 10px;
-    margin: 20px 15px;
+    padding: 30px;
+  }
+
+  a {
+    cursor: pointer;
+    color: var(--ucd-blue);
+    font-weight: bold;
+    text-decoration: underline;
+  }
+
+  p {
+    margin: 8px 0;
+    line-height: 23px;
   }
 
   pre {
@@ -18,48 +29,118 @@ return html`
 
   h2 {
     margin-top: 0;
+    font-size: 28px;
+    color: #191919;
   }
   h3 {
-    margin-bottom: 5px;
+    margin-top: 24px;
+    margin-bottom: 4px;
+    font-size: 18px;
+    color: #191919;
   }
   h4 {
     margin-bottom: 5px;
   }
 
+  #description {
+    display: flex;
+    flex-direction: row;
+    margin: 8px 0;
+    line-height: 23px;
+  }
+
+  #content-wrapper {
+    display: flex;
+    flex-direction: row;
+  }
+
+ .content {
+    margin-right: 20px;
+  }
+
+  .image {
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+    border-radius: 50%;
+    width: 75px !important;
+    height: 70px;
+    background-color: #008EAA;
+  }
+
+  .image .icon {
+    margin: 0 auto;
+    height: 40px;
+  }
+
+  iron-icon.description {
+    padding-right: 4px;
+    color: #B3C1D1;
+    font-size: 24px;
+  }
+
   .info-group {
-    margin-left: 15px;
+    font-size: 16px;
+    color: #191919;
+  }
+
+  .info-group > div {
+    margin: 6px 0;
+  }
+
+  table {
+    margin: 18px 0 24px 0;
+    border-collapse: collapse;
+  }
+  table tr td {
+    padding-bottom: 6px;
+  }
+  table tr td:nth-child(2) {
+    padding-left: 10px;
+    font-weight: bold;
+  }
+
+  h3 > iron-icon {
+    font-size: 24px;
+    color: #FFBF00;
+    transform: rotate(-90deg);
+    cursor: pointer;
   }
 
   .connection-examples {
     border-left: 2px solid var(--ucd-blue);
     padding-left: 13px;
   }
-
-  table {
-    margin-top: 15px;
-    border-collapse: collapse;
-  }
-  table tr td {
-    padding-bottom: 3px;
-  }
-  table tr td:nth-child(2) {
-    padding-left: 10px;
-    color: var(--ucd-blue)
-  }
-
-  a {
-    cursor: pointer;
-    color: var(--ucd-blue);
-  }
 </style>  
 
-<div>
-  <h2>${this.database.CLUSTER_NAME}</h2>
+<div id="content-wrapper">
+  <div class="content">
+    <div class="left-side-content">
+      <h2>${this.database.CLUSTER_NAME}</h2>
+    
+      <!--- Description --->
+      <p>
+        4km grid, 10 year average weather data for Pacific 
+        northwest. Used by the Poplar Model web application.
+      </p>
 
-  <div 
-    ?hidden="${!this.database.DESCRIPTION}" 
-    class="description"
-    id="description">
+      <div 
+        ?hidden="${!this.database.DESCRIPTION}" 
+        class="description"
+        id="description">
+      </div>
+
+      <div>
+        <iron-icon class="description" icon="description"></iron-icon>
+          <a href="">
+            View Documentation
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="image">
+      <img class="icon" src="/images/cloud-sun-solid.svg">
+    </div>
   </div>
 
   <h3>Rest API Access via <a href="http://postgrest.org/en/v7.0.0/api.html" target="_blank">PostgREST</a></h3>
@@ -72,7 +153,11 @@ return html`
 
   <h3>Database Connection Information</h3>
   <div class="info-group">
-    <div>PostgreSQL Version: ${this.database.PG_VERSION}</div>
+    <div>
+      <em>
+        PostgreSQL Version: ${this.database.PG_VERSION}
+      </em>
+    </div>
     <table>
       <tr>
         <td>Host</td>
@@ -97,7 +182,12 @@ return html`
     </table>
   </div>
 
-  <h3><a @click="${this._onConnectionToggleClicked}" class="connection-examples-toggle">Connection Examples</a></h3>
+  <h3>
+    <iron-icon class="arrow-drop-down" icon="arrow-drop-down"></iron-icon>
+    <a @click="${this._onConnectionToggleClicked}" class="connection-examples-toggle">
+      Connection Examples
+    </a>
+  </h3>
   <div ?hidden="${!this.connectionHelpOpen}" class="connection-examples">
     <h4>JDBC Connection String</h4>
     <div class="info-group">
@@ -142,10 +232,8 @@ return html`
   # frame = pd.read_sql(sql_command, conn)</pre>
     </div>
 
-
     <h4>SSL</h4>
     <div>Note: Only secure SSL connections are allowed to the database</div>
   </div>
 </div>
-
 `;}
--- client/public/elements/app-search-box.tpl.js
+++ client/public/elements/app-search-box.tpl.js
@@ -8,29 +8,63 @@ export default function render() {
         max-width: 900px;
       }
 
-      #search-bar-wrapper {
+      #search-input-wrapper {
         display: flex;
         flex-direction: row;
         flex-wrap: nowrap;
         align-items: center;
-        padding: 30px 0;
+        padding: 30px;
         height: 44px;
       }
 
-      #search-bar-wrapper > label {
+      #search-input-wrapper > .input-container {
+        display: flex;
+        flex-wrap: wrap;
         width: 100%;
         position: relative;
+        align-items: stretch;
       }
 
-      #search-bar {
+      #search-input {
+        display: block;
+        height: calc(1.5em + .75rem);
+        padding: .375rem .75rem;
         margin: 0 10px;
-        width: 100%;
+        width: 1%;
         max-width: 600px;
         height: 44px;
         font-size: 16px;
+        font-weight: 400;
+        line-height: 1.5;
+        background-color: white;
+        border: 1px solid #ced4da;
+        border-radius: .25rem;
+        position: relative;
+        flex: 1 1 auto;
+        min-width: 0;
+        margin-bottom: 0;
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
       }
 
-      #search-bar-wrapper > .sort-by {
+      #search-input-button {
+        display: inline-block;
+        border: 1px solid transparent;
+        position: relative;
+        top: -10px;
+        left: -32px;
+        z-index: 2;
+        background-color: transparent;
+      }
+
+      #search-input-button > iron-icon {
+        position: absolute;
+        right: 0;
+        color: #355B85;
+        font-size: 24px;
+      }
+
+      #search-input-wrapper > .sort-by {
         padding-right: 10px;
         color: #022851;
         font-size: 14px;
@@ -61,30 +95,19 @@ export default function render() {
         appearance: none;
       }
 
-      .search-bar-button {
-        
-      }
-
-      .search-icon {
-        position: absolute;
-        right: 0;
-        color: #355B85;
-        font-size: 24px;
-      }
-
       .expand-more {
         color: #FFBF00;
         background-color: #002851;
       }
     </style>
 
-    <div id="search-bar-wrapper">
-      <label>
-        <input type="text" id="search-bar" name="search-bar" placeholder="Search databases" value="">
-        <button class="search-bar-button" type="submit">
+    <div id="search-input-wrapper">
+      <div class="input-container">
+        <input type="text" id="search-input" name="search-input" placeholder="Search databases" value="">
+        <button id="search-input-button" type="button">
           <iron-icon class="search-icon" icon="search"></iron-icon>
         </button>
-      </label>
+      </div>
       <span style="width: 30px;"></span>
       <span class="sort-by">Sort By</span>
       <div class="search-dropdown-wrapper">
